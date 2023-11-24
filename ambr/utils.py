4e9ccedfca44803ae4b95a4439d9355e07193d44
seriaati <seria.ati@gmail.com>
2023-11-24 21:39:07 +0800

    fix: replace \\n with \n

--- ambr/utils.py
+++ ambr/utils.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 
 def remove_html_tags(text: str) -> str:
     clean = re.compile(r"<.*?>|\{SPRITE_PRESET#[^\}]+\}")
-    return re.sub(clean, "", text)
+    return re.sub(clean, "", text).replace("\\n", "\n")
 
 
 def replace_placeholders(string: str, params: Dict[str, Any]) -> str:
